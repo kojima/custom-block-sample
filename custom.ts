5b70d70ca9b774f643b1e363eac42d2281da378f
kojima <hide.k0712@gmail.com>
2020-12-31 17:25:46 +0900

    Improve custom block usability

--- custom.ts
+++ custom.ts
@@ -2,26 +2,27 @@ enum LEDs {
     //% block="LED 1"
     LED1,
     //% block="LED 2"
-    LED2
+    LED2,
+    //% block="LED 1,2"
+    BOTH_LEDS
 }
+
+let t5geStrip1: neopixel.Strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+let t5geStrip2: neopixel.Strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
 
 /**
  * 津田小5年ブロック
  */
 //% weight=70 color=#e67e22 icon="\uf005" block="津田小5年ブロック"
 namespace tsuda_5th_grade_event {
-    let strip1: neopixel.Strip = null;
-    let strip2: neopixel.Strip = null;
-
-    //% block="LEDセットアップ"
-    export function setupLEDs() {
-        strip1 = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB);
-        strip2 = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB);
-    }
 
     //% block="%led|を%color|で点灯"
     export function litLED(led: LEDs, color: NeoPixelColors) {
-        let strip = led == LEDs.LED1 ? strip1 : strip2;
-        strip.showColor(neopixel.colors(color));
+        if (led == LEDs.LED1 || led == LEDs.BOTH_LEDS) {
+            t5geStrip1.showColor(neopixel.colors(color));
+        }
+        if (led == LEDs.LED2 || led == LEDs.BOTH_LEDS) {
+            t5geStrip2.showColor(neopixel.colors(color));
+        }
     }
 }
