8ffd4c0f468f69c7dd67c3accd8b67fdc7fabad9
kojima <hide.k0712@gmail.com>
2021-05-16 10:49:33 +0900

    Changes for 2021 event

--- custom.ts
+++ custom.ts
@@ -25,28 +25,30 @@ enum NeoPixelColorsPlus {
     None = null
 }
 
-enum LEDs {
-    //% block="LED 1"
-    LED1,
-    //% block="LED 2"
-    LED2,
-    //% block="LED 1,2"
-    BOTH_LEDS
+enum MusicTitle {
+    //% block="Step and a step"
+    STEP_AND_A_STEP = 'step_and_a_step',
+    //% block="群青"
+    GUNJO = 'gunjo'
 }
 
-enum Offsets {
-    //% block="1"
-    ONE = 1,
-    //% block="2"
-    TWO = 2,
+enum Palette {
+    //% block="パレット1"
+    Palette1 = 0,
+    //% block="パレット2"
+    Palette2 = 1,
+    //% block="パレット3"
+    Palette3 = 2,
+    //% block="パレット4"
+    Palette4 = 3,
+    //% block="パレット5"
+    Palette5 = 4,
+    //% block="パレット6"
+    Palette6 = 5,
 }
 
-enum RotateDirection {
-    //% block="前"
-    FORWRD = 1,
-    //% block="後"
-    BACKWARD = -1,
-}
+type Dict = {[key: string]: [number]};
+const paletteColors: Dict = {};
 
 let t5gpStrip1: neopixel.Strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 let t5gpStrip2: neopixel.Strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
@@ -58,111 +60,75 @@ let t5gpStrip2: neopixel.Strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.
 namespace tsuda_5th_grade_performance {
 
     /**
-     * LEDを指定した色で点灯します
+     * パレットを指定した色に設定します
     */
-    //% block="%led|を%color=neopixel_colors_plus|で点灯"
+    //% block="%musicTitle=music_title|の%palette=palette|を%color=neopixel_colors_plus|にする"
     //% weight=100
-    export function litLED(led: LEDs, color: number): void {
+    export function setPaletteColor(musicTitle: MusicTitle, palette: Palette, color: number): void {
+        if (!paletteColors[musicTitle]) {
+            paletteColors[musicTitle] = [
+                NeoPixelColorsPlus.None,
+                NeoPixelColorsPlus.None,
+                NeoPixelColorsPlus.None,
+                NeoPixelColorsPlus.None,
+                NeoPixelColorsPlus.None,
+                NeoPixelColorsPlus.None
+            ]
+        }
+        paletteColors[musicTitle][palette] = color
+    }
+
+    function _litLED(color: number): void {
         if (color === NeoPixelColorsPlus.None) {
-            turnOffLED(led)
+            _turnOffLED()
         } else {
-            if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip1.showColor(color)
-            }
-            if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip2.showColor(color)
-            }
+            t5gpStrip1.showColor(color)
+            t5gpStrip2.showColor(color)
         }
     }
 
-    function _setPixelColor(led: LEDs, offset: number, color: number): void {
+    function _setPixelColor(offset: number, color: number): void {
         if (color === null) {
-            if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip1.buf.fill(0, offset * 3, 3)
-            }
-            if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip2.buf.fill(0, offset * 3, 3)
-            }
+            t5gpStrip1.buf.fill(0, offset * 3, 3)
+            t5gpStrip2.buf.fill(0, offset * 3, 3)
         } else {
-            if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip1.setPixelColor(offset, color)
-            }
-            if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-                t5gpStrip2.setPixelColor(offset, color)
-            }
+            t5gpStrip1.setPixelColor(offset, color)
+            t5gpStrip2.setPixelColor(offset, color)
         }
     }
 
-    /**
-     * LEDを別々の色で点灯します
-    */
-    //% block="%led|を%color1=neopixel_colors_plus|%color2=neopixel_colors_plus|%color3=neopixel_colors_plus|で点灯"
-    //% weight=90
-    export function litLEDWithColors(led: LEDs, color1: number, color2: number, color3: number): void {
-        _setPixelColor(led, 0, color1)
-        _setPixelColor(led, 1, color2)
-        _setPixelColor(led, 2, color3)
-        if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip1.show()
-        }
-        if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip2.show()
-        }
+    function _litLEDWithColors(color1: number, color2: number, color3: number): void {
+        _setPixelColor(0, color1)
+        _setPixelColor(1, color2)
+        _setPixelColor(2, color3)
+        t5gpStrip1.show()
+        t5gpStrip2.show()
     }
 
-    /**
-     * LEDを消灯します
-    */
-    //% block="%led|を消灯"
-    //% weight=80
-    export function turnOffLED(led: LEDs): void {
-        if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip1.clear()
-            t5gpStrip1.show()
-        }
-        if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip2.clear()
-            t5gpStrip2.show()
-        }
+    function _turnOffLED(): void {
+        t5gpStrip1.clear()
+        t5gpStrip1.show()
+        t5gpStrip2.clear()
+        t5gpStrip2.show()
     }
 
-    /**
-     * LEDの光っている位置をずらします
-    */
-    //% block="%led|を%direcastion|へ%offset|個ずらす"
-    //% weight=70
-    export function rotate(led: LEDs, direction: RotateDirection, offset: Offsets): void {
-        const d = direction === RotateDirection.FORWRD ? 1 : -1
-        const o = offset === Offsets.ONE ? 1 : 2
-        if (led === LEDs.LED1 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip1.rotate(d * o)
-            t5gpStrip1.show()
-        }
-        if (led === LEDs.LED2 || led === LEDs.BOTH_LEDS) {
-            t5gpStrip2.rotate(d * o)
-            t5gpStrip2.show()
-        }
-    }
-
-    /**
-     * LEDの色を選択します
-    */
-    //% weight=10
-    //% blockId="neopixel_colors_plus" block="%color"
-    //% advanced=true
-    export function colors(color: NeoPixelColorsPlus): number {
+    function colors(color: NeoPixelColorsPlus): number {
         return color
     }
 
     /**
      * カラーコード(#FF00FFのようなコード)を色に変換します
     */
-    //% weight=5
+    //% weight=10
     //% block="カラーコード%colorCode|を色に変換"
-    //% advanced=true
     export function convertColorCode(colorCode: string): number {
-        if (colorCode[0] === '#') {
-            colorCode = colorCode.slice(1)
+        let sanitized = false
+        while (!sanitized) {
+            if (colorCode.length > 0 && (colorCode[0] === '#' || colorCode[0] === ' ')) {
+                colorCode = colorCode.slice(1)
+            } else {
+                sanitized = true
+            }
         }
         return parseInt(colorCode, 16)
     }
